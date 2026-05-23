f907c41238067eae97de1ddfbe6afd4010628f3a
lukasenka <starkus.lukas@gmail.com>
2026-05-23 15:34:38 +0300

    Db truktura. Finaline versija

--- src/main/resources/db.changelog/db.changelog-movie.sql
+++ src/main/resources/db.changelog/db.changelog-movie.sql
@@ -2,7 +2,7 @@ CREATE TABLE movie (
     movie_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     rated_id BIGINT NOT NULL,
     title VARCHAR(255) NOT NULL,
-    year VARCHAR(10) NOT NULL,
+    movie_year VARCHAR(10) NOT NULL,
     released VARCHAR(50) NOT NULL,
     runtime VARCHAR(50) NOT NULL,
     plot VARCHAR(2000) NOT NULL,
