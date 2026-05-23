fe7ecda1a75e9ba293217ca9547180ee15b9dd3a
Dominiklt84 <dominik.alencinovic@stud.viko.lt>
2026-05-23 13:43:35 +0300

    Pradžia projetko

---
+++ src/main/java/lt/viko/eif/dalencinovic/lstarkus/tsaviscevas/KinoFilmai/KinoFilmaiApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.dalencinovic.lstarkus.tsaviscevas.KinoFilmai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoFilmaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinoFilmaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/viko/eif/dalencinovic/lstarkus/tsaviscevas/KinoFilmai/KinoFilmaiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.dalencinovic.lstarkus.tsaviscevas.KinoFilmai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoFilmaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
